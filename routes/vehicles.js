c412fa0ccaa79ea9a951aa26784d418a2165bf25
RakshithaBoddireddy <S564570@nwmissouri.edu>
2023-11-15 21:04:34 -0600

    vehicles added

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,15 +1,15 @@
 var express = require('express');
-const vehicles_controllers= require('../controllers/vehicles');
+var vehicles_controller= require('../controllers/vehicles');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', vehicles_controllers.vehicles_view_all_Page );
+router.get('/', vehicles_controller.vehicles_view_all_Page );
 /* GET detail vehicles page */
-router.get('/detail', vehicles_controllers.vehicles_view_one_Page);
+router.get('/detail', vehicles_controller.vehicles_view_one_Page);
 /* GET create vehicle page */
-router.get('/create', vehicles_controllers.vehicles_create_Page);
+router.get('/create', vehicles_controller.vehicles_create_Page);
 /* GET create update page */
-router.get('/update', vehicles_controllers.vehicle_update_Page);
+router.get('/update', vehicles_controller.vehicle_update_Page);
 /* GET delete costume page */
-router.get('/delete', vehicles_controllers.vehicles_delete_Page);
+router.get('/delete', vehicles_controller.vehicles_delete_Page);
 module.exports = router;
