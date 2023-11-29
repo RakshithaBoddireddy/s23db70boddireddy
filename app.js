95876a2929e198ecd2c55f2a605125828dd3855c
RakshithaBoddireddy <S564570@nwmissouri.edu>
2023-11-28 22:24:04 -0600

    login.pug added

--- app.js
+++ app.js
@@ -45,8 +45,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/vehicles', vehiclesRouter);
