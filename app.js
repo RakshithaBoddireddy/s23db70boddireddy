ccd8b963f64d74f0a4be13c49ba3d6499723f598
RakshithaBoddireddy <S564570@nwmissouri.edu>
2023-11-28 22:25:21 -0600

    login.pug added

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ mongoose.connect(connectionString);
 // passport config
 // Use the existing connection
 // The Account model
-var Account =require('./models/account'));
+var Account =require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
