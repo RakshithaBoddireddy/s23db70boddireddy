38be5454d70f966da6fe32e2449d44e4a9ed58b0
RakshithaBoddireddy <S564570@nwmissouri.edu>
2023-11-29 18:29:27 -0600

    Bonus added

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -15,11 +15,11 @@ res.redirect("/login");
 /* GET home page. */
 router.get('/', vehicles_controller.vehicles_view_all_Page );
 /* GET detail vehicles page */
-router.get('/detail', vehicles_controller.vehicles_view_one_Page);
+router.get('/detail', secured, vehicles_controller.vehicles_view_one_Page);
 /* GET create vehicle page */
-router.get('/create', vehicles_controller.vehicles_create_Page);
+router.get('/create', secured, vehicles_controller.vehicles_create_Page);
 /* GET create update page */
 router.get('/update', secured, vehicles_controller.vehicles_update_Page);
 /* GET delete costume page */
-router.get('/delete', vehicles_controller.vehicles_delete_Page);
+router.get('/delete', secured, vehicles_controller.vehicles_delete_Page);
 module.exports = router;
